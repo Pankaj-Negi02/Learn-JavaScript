fce81f5e360b83c865983d18f667c057fca8a910
Pankaj Negi <97275626+Pankaj-Negi02@users.noreply.github.com>
2024-09-15 07:50:02 +0000

    Datatype

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,10 @@
+var name = "Pankaj";
+name = "Negi";
+
+let email = "pankajnegi02@gmail.com";
+email = "pankaj.negi@aajtak.com";
+
+const city = "Ghaziabad";
+//city = "Pauri Garhwal";
+
+console.table([name, email, city]);
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict";
+
+let name = "Pankaj";
+let age = 30;
+let isLogedIn = true;
+let state; 
+let email = null;
+
+// number => 2 to power 53
+// bigint = >
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof email);
